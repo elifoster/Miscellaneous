eaf3b15d6822bb5aa7d72abc560359c059c424c9
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2017-03-08 22:49:31 -0800

    Rewrite navboxtimetable using parse API
    No longer consider all Lua navboxes lazy and all non-Lua navboxes non-lazy.

--- wiki/navboxtimetable.rb
+++ wiki/navboxtimetable.rb
@@ -1,30 +1,61 @@
-require 'oocsv'
+require 'mediawiki/butt'
 
-def time(value)
-  color = 'green'
-  if value.to_f > 2
-    color = 'red'
-  elsif value.to_f > 3
-    color = 'darkred'
+@mw = MediaWiki::Butt.new('https://ftb.gamepedia.com/api.php', query_limit_default: 'max', assertion: :bot, use_continuation: true)
+@mw.login('username', 'password')
+
+def parse(title)
+  params = {
+    action: 'parse',
+    page: title,
+    contentmodel: 'json',
+    prop: 'limitreportdata'
+  }
+  data = @mw.post(params)['parse']['limitreportdata']
+  cpu_time = data.select { |data_piece| data_piece['name'] == 'limitreport-cputime' }[0]['0'].to_f
+  real_time = data.select { |data_piece| data_piece['name'] == 'limitreport-walltime' }[0]['0'].to_f
+  ReportData.new(cpu_time, real_time)
+end
+
+navboxes = @mw.get_category_members('Mod navigation templates')
+str = %({|class="wikitable sortable"\n)
+str << "! Navbox !! CPU Time !! Real Time !! Language !! Lazy?\n"
+navboxes.each do |navbox|
+  str << "|-\n"
+  content = @mw.get_text(navbox)
+  is_lazy = content.start_with?('{{Navbox/Lazy')
+  if is_lazy
+    content_content = @mw.get_text("#{navbox}/content")
+    is_lua = content_content.start_with?('{{Navbox/Lazy/content') # This template does the #invoke thing as described below
+  else
+    is_lua = content.start_with?('{{#invoke:Navbox')
   end
   
-  "<span style=\"color: #{color}\">#{value} seconds</span>"
+  report = parse(navbox)
+  str << "| {{Tl|#{navbox.sub('Template:', '')}}} || #{report.cpu_time} || #{report.real_time} || #{is_lua ? 'Lua' : 'Wikitext'} || #{is_lazy.to_s.capitalize}\n"
 end
 
-STR = <<EOF
-csv here
-EOF
+str << "|}\n"
+print str
 
-ary = OOCSV.read(STR)
-
-ret = "{|class=\"wikitable sortable\"\n"
-ret << "! Navbox !! CPU Time !! Real Time !! Lua\n"
-
-ary.each do |entry|
-  ret << "|-\n"
-  ret << "| {{Tl|Navbox #{entry.Navbox}}} || #{time(entry.CPUTime)} || #{time(entry.RealTime)} || #{entry.Lua}\n"
+class ReportData
+  attr_reader :cpu_time
+  attr_reader :real_time
+  
+  def initialize(cpu_time, real_time)
+    @cpu_time = cpu_time.wrap_with_color
+    @real_time = real_time.wrap_with_color
+  end
 end
 
-ret << '|}'
+class Float
+  def wrap_with_color
+    color = 'green'
+    if self > 3
+      color = 'darkred'
+    elsif self > 2
+      color = 'red'
+    end
 
-print ret
+    "<span style=\"color: #{color}\">#{self} seconds</span>"
+  end
+end
